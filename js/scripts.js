0c04f58897a21433389d79944e8656f4d0325057
Nicksy <kipkorirnickson45@gmai.com>
2018-01-15 11:24:28 +0300

    added scrpts.js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// code to display a hiden div:projects
+$(document).ready(function(){
+  $("a#projo").click(function (){
+    $("#projects").show();
+  });
+});
+// code to display hiden div:contacts
+$(document).ready(function(){
+  $("a#concat").click(function(){
+    $("#contacts").show();
+  });
+});
+// code to display hiden div:Education
+$(document).ready(function(){
+  $("a#educ").click(function(){
+    $("#education").show();
+  });
+});
